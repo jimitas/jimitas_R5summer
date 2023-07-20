e34c4431409892327d60b5c19cce08e6462ca03f
jimitas <nonajun805@gmail.com>
2023-07-21 06:22:36 +0900

    PopUpMenu機能変更

--- src/components/Popupmenu/index.tsx
+++ src/components/Popupmenu/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, FC } from "react";
+import { useState, useRef, FC } from "react";
 import Links from "src/components/Links/index";
 import styles from "src/components/Popupmenu/Popupmenu.module.css";
 
@@ -6,12 +6,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faList } from "@fortawesome/free-solid-svg-icons";
 import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
 
+
+//コンテンツの外をクリックしたら、isShow=falseになって、ポップアップが消えるようにしたい。できればでよいが…。
 export const PopupMenu: FC = () => {
   const [isShown, setIsShown] = useState(false);
   const popupRef = useRef<HTMLDivElement>(null);
 
   const handleToggleButtonClick = () => {
-    if (isShown) return;
     setIsShown(true);
   };
 
