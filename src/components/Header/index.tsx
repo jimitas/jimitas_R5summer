9a39a284258ff7504ed91a97f10c34c474494ec8
jimitas <nonajun805@gmail.com>
2023-07-20 17:35:41 +0900

    Home Back Reload画面の設置

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,63 +1,62 @@
 // import * as se from "src/components/se";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUndo, faHome, faPalette, faLongArrowAltLeft } from "@fortawesome/free-solid-svg-icons";
-// import Link from "next/link";
 import { useChangeColor } from "src/hooks/useChangeColor";
+import { faUndo, faHome, faPalette, faLongArrowAltLeft } from "@fortawesome/free-solid-svg-icons";
+import router from "next/router";
+import Link from "next/link";
 // import { useChangeUser } from "src/hooks/useChangeUser";
-// import { useRouter } from "next/router";
 // import { PopupMenu } from "src/components/Popupmenu";
 
 import styles from "src/components/Header/header.module.css";
 
 export function Header() {
-
   const { colorIndex, changeColor } = useChangeColor();
 
   // const { GAKUNEN, KUMI, BANGO, gakunen, kumi, bango, changeGakunen, changeKumi, changeBango } = useChangeUser();
   // const router = useRouter();
 
-  // const reload = () => {
-  //   se.set.play();
-  //   const result = window.confirm("もういちど　ページを　よみこみますか？");
-  //   if (result === false) return;
-  //   location.reload();
-  // };
+  const reload = () => {
+    // se.set.play();
+    const result = window.confirm("もういちど　ページを　よみこみますか？");
+    if (result === false) return;
+    location.reload();
+  };
 
-  // const back = () => {
-  //   se.alert.play();
-  //   se.set.play();
-  //   const result = window.confirm("まえの　ページに　もどりますか？");
-  //   if (result === false) return;
-  //   router.back();
-  // };
+  const back = () => {
+    // se.alert.play();
+    // se.set.play();
+    const result = window.confirm("まえの　ページに　もどりますか？");
+    if (result === false) return;
+    router.back();
+  };
 
   return (
     <header className={styles.nav}>
-      <img className="rounded" src="../../images/jimitas_logo.png" alt="" />
-      {/* <button onClick={toggleDarkMode}>{isDarkMode ? "Light Mode" : "Dark Mode"}</button> */}
-      <div onClick={changeColor} className="w-8 cursor-pointer" style={{ color: "orange" }}>
-        <FontAwesomeIcon icon={faPalette} />
-      </div>
-
-
-      {/* <div className={styles.navButton}>
+      <div className="w-8 text-3xl font-bold mx-2 cursor-pointer">
         <Link href="./">
           <FontAwesomeIcon icon={faHome} style={{ color: "pink" }} />
         </Link>
       </div>
 
-      <div className={styles.navButton} style={{ color: "yellow" }}>
-        <PopupMenu />
+      <div onClick={changeColor} className="w-8 mx-2 cursor-pointer" style={{ color: "orange" }}>
+        <FontAwesomeIcon icon={faPalette} />
       </div>
 
-      <div onClick={back} className={styles.navButton}>
+      <div onClick={back} className="w-8 text-3xl font-bold mx-2 cursor-pointer">
         <FontAwesomeIcon icon={faLongArrowAltLeft} />
       </div>
 
-      <div onClick={reload} className={styles.navButton}>
+      <div onClick={reload} className="w-8 text-3xl font-bold mx-2 cursor-pointer">
         <FontAwesomeIcon icon={faUndo} />
       </div>
 
+      {/* 
+
+      <div className="text-3xl font-bold mx-5 cursor-pointer" style={{ color: "yellow" }}>
+        <PopupMenu />
+      </div>
+
+      
      */}
     </header>
   );
