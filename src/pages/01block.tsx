14dc0c8100f26afb4184a0b5257572faea54d6a3
jimitas <nonajun805@gmail.com>
2023-08-05 12:24:21 +0900

    blockコンテンツにHideを追加

--- src/pages/01block.tsx
+++ src/pages/01block.tsx
@@ -1,24 +1,13 @@
-// import { Layout } from "src/components/Layout/Layout";
-// import { Hide } from "src/components/Hide";
-
-// export default function Home() {
-//   return (
-//     <Layout title="ぶろっく">
-//       <Block a={10} b={10} />
-//       <Hide />
-//     </Layout>
-//   );
-// }
-
 import React from "react";
 import Layout from "@/components/Layout";
+import { Hide } from "src/components/Hide";
 import { Block } from "@/components/Block";
-// Add imports for Block and Hide components if they are used
 
 const Home: React.FC = () => {
   return (
     <Layout title="ぶろっく">
       <Block a={10} b={10} />
+      <Hide />
     </Layout>
   );
 };
