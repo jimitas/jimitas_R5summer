42348253cb2987127a1ff0a23a1ee4a95d5ee2f9
jimitas <nonajun805@gmail.com>
2023-08-06 11:45:04 +0900

    divColorがいらないので削除 画像を切り替えるか、色を変えるか

--- src/components/Block/index.tsx
+++ src/components/Block/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import Image from "next/image"; // Imageコンポーネントをインポート
 import React, { useEffect, useState, useRef } from "react";
 import * as se from "src/components/se";
@@ -9,7 +8,6 @@ import { useDragDrop } from "src/hooks/useDragDrop";
 import { BtnSpace } from "src/components/PutButton/btnSpace";
 import { BtnUndo } from "src/components/PutButton/btnUndo";
 
-const divColor = ["#ff8082", "#005aff", "#ff8082", "#005aff"];
 
 interface BlockProps {
   a: number;
@@ -46,8 +44,8 @@ export function Block(props: BlockProps) {
     se.seikai1.play();
   };
 
-  const colorChange = () => {
-    se.pi.play();
+  const colorChange = (e:any) => {
+  console.log(e.target.src)
   };
 
   //後で色を変えるイベントを追加
@@ -87,6 +85,8 @@ export function Block(props: BlockProps) {
                     ) : colIndex < putBlockCount[tableIndex * 2 + trIndex] && (tableIndex === 1 || tableIndex === 3) ? (
                       <Image
                         draggable="true"
+                        src={blockBlue}
+                        alt="blockBlue"
                         className={`draggable-elem ${styles.suuzuBlock}`}
                         onClick={colorChange}
                         onTouchStart={touchStart}
@@ -95,8 +95,6 @@ export function Block(props: BlockProps) {
                         onDragStart={dragStart}
                         onDragOver={dragOver}
                         onDrop={dropEnd}
-                        src={blockBlue}
-                        alt="blockBlue"
                       />
                     ) : null}
                   </td>
@@ -110,55 +108,3 @@ export function Block(props: BlockProps) {
     </div>
   );
 }
-
-// 現在el_tableにイベントを設置し、その子要素に伝搬する形で実現をしているが、本来の形ではない。
-// 本来は、divの子要素に直接イベントを設置するべきだが、それでもまだうまく配置できていない。
-// ブロック自体はマップ関数で配置すれば良いものだが、そこもうまく言っていない。
-// もしマップ関数を利用するならば、それぞれのどろっぱぶるに配列を用意し、そこに格納したり、
-// 配列の要素削除、追加を設定することで、ドラッグアンドドロップを実現すると考えられる。
-// ただ、数図ブロックのように、それぞれのTDに直接ドロップする形をとるならば、配列の形は
-// あまり効率的ともいえない。
-
-// まずは、カラーチェンジの要素を取り除き、ドラッグドロップができるかどうかを試したい。
-
-// ブロックを画像で表示し、回転のアニメーションをつけたい
-// タッチイベントについては直接divに配置できるようになった。
-// ドラッグイベントについては、子要素に直接イベントを付与できないが、とりあえず放置
-// 現在のスタイリングでは、例えばテーブルごとドラッグされるということはないが、ドラッグできる余白があると、勝手に動いてしまう恐れがある。
-// よく考えたら、react-draggableしか勝たん？
-
-// react-draggableだとやはり、パフォーマンスが落ちるのと、カラーチェンジやCSSの当て方がうまくいかないため、やはり却下します。
-// const handleDragStart = (e: DragEvent) => {
-//   dragStart(e as unknown as React.DragEvent<HTMLDivElement>);
-// };
-// const handleDragOver = (e: DragEvent) => {
-//   dragOver(e as unknown as React.DragEvent<HTMLDivElement>);
-// };
-// const handleDropEnd = (e: DragEvent) => {
-//   dropEnd(e as unknown as React.DragEvent<HTMLDivElement>);
-// };
-
-// const handleTouchStart = (e: TouchEvent) => {
-//   touchStart(e as unknown as React.TouchEvent<HTMLDivElement>);
-// };
-
-// const handleTouchMove = (e: TouchEvent) => {
-//   touchMove(e as unknown as React.TouchEvent<HTMLDivElement>);
-// };
-
-// const handleTouchEnd = (e: TouchEvent) => {
-//   touchEnd(e as unknown as React.TouchEvent<HTMLDivElement>);
-// };
-// div.addEventListener("click", colorChange, false);
-// div.addEventListener("touchstart", touchStartEvent, false);
-// div.addEventListener("touchend", touchEndEvent, false);
-// div.addEventListener("dragstart", handleDragStart, false);
-// div.addEventListener("dragover", handleDragOver, false);
-// div.addEventListener("drop", handleDropEnd, false);
-// div.addEventListener("touchstart", handleTouchStart, false);
-// div.addEventListener("touchmove", handleTouchMove, false);
-// div.addEventListener("touchend", handleTouchEnd, false);
-// 各ブロックに異なる数だけブロックを配置したい。
-// テーブル４つ、２行５列のブロックに、flagを挿入して、その判定によって、
-// テーブルを配置するようにしたらどうか？
-// うまく配置出来たら、imgに直接イベントを挿入できそうだ。
