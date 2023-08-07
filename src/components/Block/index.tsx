b3e171db25b7a33bd95d8da8781b913ea315262d
jimitas <nonajun805@gmail.com>
2023-08-07 10:30:21 +0900

    8/7 backupとしてとりあえず保存

--- src/components/Block/index.tsx
+++ src/components/Block/index.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image"; // Imageコンポーネントをインポート
 import React, { useEffect, useState, useRef } from "react";
 import * as se from "src/components/se";
 import styles from "src/components/Block/Block.module.css";
@@ -12,27 +11,11 @@ interface BlockProps {
 }
 
 export function Block(props: BlockProps) {
-  const TABLES = ["tableLeftUp", "tableRightUp", "tableLeftLo", "tableRightLo"];
-  const TABLE_COLUMNS = [0, 1, 2, 3, 4];
-  const TABLE_ROWS = [0, 1];
-
   const el_table_place = useRef<HTMLDivElement>(null);
   const leftUpCount: number = props.a > 10 ? 10 : 0 || 0;
   const rightUpCount: number = props.b > 10 ? 10 : 0 || 0;
   const leftLoCount: number = props.a > 10 ? props.a - 10 : props.a === 0 ? 0 : props.a || 10;
   const rightLoCount: number = props.b > 10 ? props.b - 10 : props.b === 0 ? 0 : props.b || 10;
-
-  //各テーブルのブロックの個数を管理
-  const putBlockCount: number[] = [
-    leftUpCount > 5 ? 5 : leftUpCount,
-    leftUpCount < 5 ? 0 : leftUpCount - 5,
-    rightUpCount > 5 ? 5 : rightUpCount,
-    rightUpCount < 5 ? 0 : rightUpCount - 5,
-    leftLoCount > 5 ? 5 : leftLoCount,
-    leftLoCount < 5 ? 0 : leftLoCount - 5,
-    rightLoCount > 5 ? 5 : rightLoCount,
-    rightLoCount < 5 ? 0 : rightLoCount - 5,
-  ];
 
   const [count, setCount] = useState(0);
 
@@ -42,9 +25,6 @@ export function Block(props: BlockProps) {
   };
 
   const { dragStart, dragOver, dropEnd, touchStart, touchMove, touchEnd } = useDragDrop();
-
-  useEffect(() => {
-  }, [count, leftUpCount, rightUpCount, leftLoCount, rightLoCount]);
 
   const flippedBlock = (e: any) => {
     se.pi.play();
@@ -56,60 +36,77 @@ export function Block(props: BlockProps) {
     }
   };
 
+  let touchStartFlag = false;
+  const touchStartEvent = () => {
+    touchStart;
+    //150ミリ秒以内にタッチして指を離すとき，クリックイベントと同じ挙動とみなす。
+    touchStartFlag === false ? (touchStartFlag = true) : (touchStartFlag = false);
+    setTimeout(() => {
+      touchStartFlag = false;
+    }, 150);
+  };
+  
+  useEffect(() => {
+    const touchMoveEvent = () => {
+      touchMove;
+    };
+    const touchEndEvent = () => {
+      touchEnd;
+      touchStartFlag === true ? flippedBlock : null;
+    };
+
+    const ele = el_table_place.current;
+    while (ele!.firstChild) {
+      ele!.removeChild(ele!.firstChild);
+    }
+
+    for (let tableIndex = 0; tableIndex < 4; tableIndex++) {
+      const TBL = document.createElement("table");
+      ele!.appendChild(TBL);
+      for (let rowIndex = 0; rowIndex < 2; rowIndex++) {
+        const tr = document.createElement("tr");
+        TBL.appendChild(tr);
+        for (let colIndex = 0; colIndex < 5; colIndex++) {
+          const td = document.createElement("td");
+          td.className = "droppable-elem";
+          tr.appendChild(td);
+
+          if (
+            (tableIndex === 0 && rowIndex * 5 + colIndex < leftUpCount) ||
+            (tableIndex === 1 && rowIndex * 5 + colIndex < rightUpCount) ||
+            (tableIndex === 2 && rowIndex * 5 + colIndex < leftLoCount) ||
+            (tableIndex === 3 && rowIndex * 5 + colIndex < rightLoCount)
+          ) {
+            const img = document.createElement("img");
+            img.src = "/images/block-blue.png";
+            img.className = "draggable-elem";
+            img.setAttribute("draggable", "true");
+            // img.addEventListener("touchstart", touchStartEvent, false);
+            // img.addEventListener("touchmove", touchMoveEvent, false);
+            // img.addEventListener("touchend", touchEndEvent, false);
+            // img.addEventListener("dragstart", dragStartEvent, false);
+            // img.addEventListener("dragover", dragOverEvent, false);
+            // img.addEventListener("drop", dropEndEvent, false);
+            img.addEventListener("click", flippedBlock, false);
+            td.appendChild(img);
+          }
+        }
+      }
+    }
+  }, [count, leftLoCount, leftUpCount, rightLoCount, rightUpCount]);
+
   return (
     <div className="flex justify-center flex-wrap items-end">
       <BtnSpace></BtnSpace>
-      <div ref={el_table_place} className={styles.table}>
-        {TABLES.map((table, tableIndex) => (
-          <table key={tableIndex} id={TABLES[tableIndex]}>
-            {TABLE_ROWS.map((row, trIndex) => (
-              <tr key={trIndex}>
-                {TABLE_COLUMNS.map((column, colIndex) => (
-                  <td
-                    key={colIndex}
-                    className="droppable-elem"
-                    onDragStart={dragStart}
-                    onDragOver={dragOver}
-                    onDrop={dropEnd}
-                  >
-                    {colIndex < putBlockCount[tableIndex * 2 + trIndex] && (tableIndex === 0 || tableIndex === 2) ? (
-                      // eslint-disable-next-line @next/next/no-img-element
-                      <img
-                        className={`draggable-elem ${styles.suuzuBlock}`}
-                        src="/images/block-pink.png"
-                        alt="block"
-                        draggable="true"
-                        onClick={flippedBlock}
-                        onTouchStart={touchStart}
-                        onTouchMove={touchMove}
-                        onTouchEnd={touchEnd}
-                        onDragStart={dragStart}
-                        onDragOver={dragOver}
-                        onDrop={dropEnd}
-                      />
-                    ) : colIndex < putBlockCount[tableIndex * 2 + trIndex] && (tableIndex === 1 || tableIndex === 3) ? (
-                      // eslint-disable-next-line @next/next/no-img-element
-                      <img
-                        draggable="true"
-                        src="/images/block-blue.png"
-                        alt="block"
-                        className={`draggable-elem ${styles.suuzuBlock}`}
-                        onClick={flippedBlock}
-                        onTouchStart={touchStart}
-                        onTouchMove={touchMove}
-                        onTouchEnd={touchEnd}
-                        onDragStart={dragStart}
-                        onDragOver={dragOver}
-                        onDrop={dropEnd}
-                      />
-                    ) : null}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </table>
-        ))}
-      </div>
+      <div
+        ref={el_table_place}
+        className={styles.table}
+        onDragStart={dragStart}
+        onDragOver={dragOver}
+        onDrop={dropEnd}
+        onTouchStart={touchStart}
+        onTouchEnd={touchEnd}
+      ></div>
       <BtnUndo handleEvent={resetTable}></BtnUndo>
     </div>
   );
